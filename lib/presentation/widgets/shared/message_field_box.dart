59c5f73542ed77cd127be72bf7073a6cb8af4048
Jacob <jacobismael6@gmail.com>
2025-04-02 18:58:31 -0600

    Design finished

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final FocusNode focusNode = FocusNode();
+
+    final textController = TextEditingController();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(20)
+    );
+
+    final inputDecoration = InputDecoration(
+        hintText: 'End your Message with a "??"',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon:Icon(Icons.send_and_archive_outlined),
+          onPressed: () {
+            final textValue =  textController.value.text;
+            print('button: $textValue');
+            textController.clear();
+          },
+        )
+      );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("Submit value $value");
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
